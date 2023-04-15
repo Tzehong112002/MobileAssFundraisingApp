4fbe75c3c53290dcf999d5e1d0ca248ee6b2f8ab
Tzehong112002 <tzehong112002@gmail.com>
2023-04-15 18:26:18 +0800

    version fragment add

--- app/src/main/java/com/example/mobileassfundraisingapp/MainActivity.kt
+++ app/src/main/java/com/example/mobileassfundraisingapp/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.mobileassfundraisingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.fragment.app.Fragment
+import com.example.mobileassfundraisingapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set up initial fragment
+        replaceFragment(Home())
+
         // Find the BottomNavigationView in the layout
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
@@ -17,7 +27,26 @@ class MainActivity : AppCompatActivity() {
         // Disable the third item in the menu
         bottomNavigationView.menu.getItem(2).isEnabled = false
 
+        // Set up listener for BottomNavigationView items
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.miHome -> replaceFragment(Home())
+                R.id.miProfile -> replaceFragment(profile())
+                R.id.miSettings -> replaceFragment(settings())
+            }
+            true
+        }
 
+        // Set up listener for FAB click
+        binding.fab.setOnClickListener {
+            replaceFragment(add())
+        }
+    }
 
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
