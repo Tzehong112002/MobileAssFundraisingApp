6ad347e1a6150405e44f52e03a64e311c1352c77
Tzehong112002 <tzehong112002@gmail.com>
2023-05-01 15:53:57 +0800

    home page

--- app/src/main/java/com/example/mobileassfundraisingapp/HomeFragment.java
+++ app/src/main/java/com/example/mobileassfundraisingapp/HomeFragment.java
@@ -1,37 +1,97 @@
 package com.example.mobileassfundraisingapp;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.widget.SearchView;
+
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.mobileassfundraisingapp.admin.DataClass;
-import com.example.mobileassfundraisingapp.admin.MyAdapter;
-import com.example.mobileassfundraisingapp.admin.UploadActivity;
-import com.example.mobileassfundraisingapp.admin.adminActivity;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private View view;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        // Do any other initialization or view setup here
 
+        // Get reference to your event card view
+        View eventCardView = view.findViewById(R.id.EventCard);
+        View profileCardView = view.findViewById(R.id.ProfileCard);
+        View settingsCardView = view.findViewById(R.id.SettingCard);
+        View logoutCardView = view.findViewById(R.id.LogOutCard);
 
+        // Set an OnClickListener to the event card view
+        eventCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new instance of EventFragment
+                searchFragment searchFragment = new searchFragment();
+
+                // Replace the current fragment with EventFragment
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, searchFragment);
+                fragmentTransaction.addToBackStack(null);  // Optional: Add to back stack
+                fragmentTransaction.commit();
+            }
+        });
+
+        profileCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new instance of EventFragment
+                profile profile = new profile();
+
+                // Replace the current fragment with EventFragment
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, profile);
+                fragmentTransaction.addToBackStack(null);  // Optional: Add to back stack
+                fragmentTransaction.commit();
+            }
+        });
+
+        settingsCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new instance of EventFragment
+                settingFragment settingFragment = new settingFragment();
+
+                // Replace the current fragment with EventFragment
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, settingFragment);
+                fragmentTransaction.addToBackStack(null);  // Optional: Add to back stack
+                fragmentTransaction.commit();
+            }
+        });
+
+        logoutCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to start the LoginActivity
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                // Clear the activity stack and start the LoginActivity
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
 }
+
--- app/src/main/java/com/example/mobileassfundraisingapp/PaymentActivity.kt
+++ app/src/main/java/com/example/mobileassfundraisingapp/PaymentActivity.kt
@@ -7,7 +7,6 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import java.text.SimpleDateFormat
 import java.util.*
