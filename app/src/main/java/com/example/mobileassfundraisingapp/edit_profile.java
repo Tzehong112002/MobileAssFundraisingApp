252f511c2182878cb164ef65eaa5b4f6d0dcd028
Tzehong112002 <tzehong112002@gmail.com>
2023-05-02 16:39:51 +0800

    validation profile page

--- app/src/main/java/com/example/mobileassfundraisingapp/edit_profile.java
+++ app/src/main/java/com/example/mobileassfundraisingapp/edit_profile.java
@@ -1,5 +1,6 @@
 package com.example.mobileassfundraisingapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -18,14 +19,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class edit_profile extends Fragment {
-    EditText editName, editEmail, editUsername, editPassword;
+    EditText editName, editEmail, cfmPassword, editPassword;
     Button saveButton;
     String nameUser, emailUser, usernameUser, passwordUser;
     DatabaseReference reference;
+    FirebaseAuth mAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,14 +38,28 @@ public class edit_profile extends Fragment {
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
 
         reference = FirebaseDatabase.getInstance().getReference("users");
+        mAuth = FirebaseAuth.getInstance();
         editName = view.findViewById(R.id.editName);
         editEmail = view.findViewById(R.id.editEmail);
-        editUsername = view.findViewById(R.id.editUsername);
+
         editPassword = view.findViewById(R.id.editPassword);
+        cfmPassword = view.findViewById(R.id.confirmPasswordEdit);
+
         saveButton = view.findViewById(R.id.saveButton);
 
         saveButton.setOnClickListener(view1 -> {
-            if (isNameChanged() || isPasswordChanged() || isEmailChanged()){
+            // Check if password confirmation matches
+            String password = editPassword.getText().toString();
+            String confirmPassword = cfmPassword.getText().toString();
+            if (!password.equals(confirmPassword)) {
+                cfmPassword.setError("Password does not match");
+                return; // password doesn't match, stay on the page
+            }
+
+            boolean isNameChanged = isNameChanged();
+            boolean isPasswordChanged = isPasswordChanged();
+            boolean isEmailChanged = isEmailChanged();
+            if (isNameChanged || isPasswordChanged || isEmailChanged) {
                 Toast.makeText(getActivity(), "Saved", Toast.LENGTH_SHORT).show();
 
                 // Navigate back to the profile fragment
@@ -51,11 +69,12 @@ public class edit_profile extends Fragment {
                 fragmentTransaction.replace(R.id.fragment_container, profileFragment);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
-
             } else {
                 Toast.makeText(getActivity(), "No Changes Found", Toast.LENGTH_SHORT).show();
             }
         });
+
+
 
         return view;
     }
@@ -76,25 +95,42 @@ public class edit_profile extends Fragment {
         }
     }
 
+
+
     private boolean isEmailChanged() {
         if (!emailUser.equals(editEmail.getText().toString())){
-            reference.child(usernameUser).child("email").setValue(editEmail.getText().toString());
-            emailUser = editEmail.getText().toString();
-            return true;
-        } else {
-            return false;
+            if(isValidEmail(editEmail.getText().toString())){
+                reference.child(usernameUser).child("email").setValue(editEmail.getText().toString());
+                emailUser = editEmail.getText().toString();
+                return true;
+            }else{
+                editEmail.setError("Invalid email format");
+            }
         }
+        return false;
     }
 
     private boolean isPasswordChanged() {
         if (!passwordUser.equals(editPassword.getText().toString())){
-            reference.child(usernameUser).child("password").setValue(editPassword.getText().toString());
-            passwordUser = editPassword.getText().toString();
-            return true;
-        } else {
-            return false;
+            String password = editPassword.getText().toString();
+            String confirmPassword = cfmPassword.getText().toString();
+            if(password.equals(confirmPassword) && isValidPassword(password)){
+                reference.child(usernameUser).child("password").setValue(password);
+                passwordUser = password;
+                return true;
+            }else{
+                if(!password.equals(confirmPassword)){
+                    cfmPassword.setError("Password does not match");
+                    return false; // password doesn't match, stay on the page
+                }
+                if(!isValidPassword(password)){
+                    editPassword.setError("Password must be at least 6 characters long");
+                }
+            }
         }
+        return false;
     }
+
 
     public void showData(){
         Intent intent = getActivity().getIntent();
@@ -104,7 +140,23 @@ public class edit_profile extends Fragment {
         passwordUser = intent.getStringExtra("password");
         editName.setText(nameUser);
         editEmail.setText(emailUser);
-        editUsername.setText(usernameUser);
         editPassword.setText(passwordUser);
     }
+
+    public boolean isValidEmail(CharSequence target) {
+        if (target == null) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+
+    public boolean isValidPassword(CharSequence target){
+        if(target == null){
+            return false;
+        }else{
+            return target.length() >= 6;
+        }
+    }
 }
+
